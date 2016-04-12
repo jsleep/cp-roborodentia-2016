d974bcfea8c18c4d67b404f20fd1714643648153
Jon Sleep <jsleep@calpoly.edu>
2016-04-12 16:06:02 -0700

    Added files via upload

---
+++ roborodentia/roborodentia.ino
@@ -0,0 +1,28 @@
+#include <QTRSensors.h>
+#include <RoboShield.h>
+#include <RoboShield_Defines.h>
+
+QTRSensorsRC qtr((unsigned char[]) {31,33,35,37,39,41,43,45}, 8);
+RoboShield rs(0);
+
+int ledPin = 22;                 // LED connected to digital pin 13
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+}
+
+void loop() {
+  unsigned int sensors[8];               
+  int position = qtr.readLine(sensors);
+  for(int i = 0; i < 8; i++){
+    Serial.print("Position ");
+    Serial.print(i);
+    Serial.print(" == ");
+    Serial.print(sensors[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  
+}
