186cbc35e93cfbefaa5c5b48c594c96eef3178d6
Jon Sleep <jsleep@calpoly.edu>
2016-04-15 18:34:15 -0700

    Added files via upload

--- robo.ino/robo.ino.ino
+++ robo.ino/robo.ino.ino
@@ -4,20 +4,24 @@
 #include <Wire.h>
 #define rightMotor 2
 #define leftMotor 3
-#define ONBLACK 250
-#define SENSOR_COUNT 8
+#define ON_BLACK 250
+#define NUM_SENSORS 8
 
 #define ON_RIGHT 500
 #define SMOOTH_RIGHT 1500
 #define ON_LEFT -500
 #define SMOOTH_LEFT -1500
 
+#define SIX_IN 505
+#define THREE_IN 235
 
 QTRSensorsRC qtr((unsigned char[]) {31, 33, 35,37,39,41, 43, 45}, 8);
 RoboShield rs(0);
 
 int ledPin = 22;                 // LED connected to digital pin 13
 
+unsigned int calibratedMinimumOn[] = {212,264,260,260,256,260,260,312};//{212,260,212,212,212,260,264,364};
+unsigned int calibratedMaximumOn[] = {4000,4000,4000,4000,4000,4000,4000,4000};//{4000,3856,3688,3076,2740,3584,4000,4000};
 void setup() {
   if (rs.buttonPressed()) {
     rs.debuggingMode();
@@ -28,89 +32,81 @@ void setup() {
   Serial.begin(9600);
   //pinMode(ledPin, OUTPUT);      // sets the digital pin as output
 
-  int i;
-  rs.lcdPrintf("Calibrating...");
-  /*for (i = 0; i < 125; i++)  // make the calibration take about 5 seconds
-  {
-    qtr.calibrate();
-    delay(20);
-  }
-  rs.lcdClear();*/
-  rs.lcdPrintf("Finished.");
+  qtr.calibratedMinimumOn = calibratedMinimumOn;
+  qtr.calibratedMaximumOn = calibratedMaximumOn;
 }
 
-void FollowLine(unsigned int distance, int position) {
+byte FollowLine(int position, unsigned int sensors[8]) {
   
   int error = position - 3350;
  
-  int sLM = 20;
-  int sRM = -20;
+  int sLM = 19;
+  int sRM = -17;
 
- /* static byte done = 1;
+  static byte done = 1;
   if (done) {
     done = 0;
-    roboShield.resetEncoder(ENCODER_0_PIN);
-    roboShield.resetEncoder(ENCODER_1_PIN);
-  } */
+    rs.resetEncoder(ENCODER_0_PIN);
+    rs.resetEncoder(ENCODER_1_PIN);
+  } 
 
   if(error < 0) { // left of line, speed up left motor
-    sRM -= ((double) abs(error) ) / 3350 * 80;
+    sRM -= ((double) abs(error) ) / 3350 * 10;
   }
   else if(error > 0) { // right of line, speed up right motor 
-    sLM += ((double) abs(error)) / 3350 * 80;
+    sLM += ((double) abs(error)) / 3350 * 10;
   }
-  
-  /*if (error >= ON_RIGHT) // line is to the right
-    sRM = 0;
-  if (error >= ON_RIGHT && error <= SMOOTH_RIGHT)
-    sRM = -10;
-  if (error <= ON_LEFT) // line is to the left
-    sLM = 0;
-  if (error <= ON_LEFT && error >= SMOOTH_LEFT)
-    sLM = 10; */
-
-  if (rs.readEncoder(ENCODER_0_PIN) > distance || rs.readEncoder(ENCODER_1_PIN) > distance) {
-    sLM = 0;
-    sRM = 0;
-  }
-
+ 
   Serial.print("L: ");
   Serial.println(sLM);
   Serial.print("R: ");
   Serial.println(sRM);
-    
+  
+  byte sensor_count = 0;
+  
+  for(int i = 0; i < NUM_SENSORS; i++) {
+    Serial.print("[");
+    Serial.print(i);
+    Serial.print("]=");
+    Serial.print(sensors[i]);
+    Serial.print(" ");
+    if(sensors[i] > ON_BLACK)
+      sensor_count++;
+  }
+  Serial.println();
+  if(sensor_count >= 3){
+    Serial.println("Hit fork");
+    sRM = 0;
+    sLM = 0;
+    rs.setMotor(rightMotor, sRM);
+    rs.setMotor(leftMotor, sLM);
+    done = 1;
+    return 0;
+  }
+  //RIGHT == MOTOR 2 == ENCODER 0
+  //LEFT == MOTOR 3 == ENCODER 1
+  /*if(rs.readEncoder(ENCODER_1_PIN) - rs.readEncoder(ENCODER_0_PIN) > 10) { //potential encoder correction
+    sLM--;
+  }*/
+  
   rs.setMotor(rightMotor, sRM);
   rs.setMotor(leftMotor, sLM);
-}
-/*
-int driveDistance(int Emotor, int distance)
-{
-  static byte done = 1;
-  if (done) {
-    done = 0;
-    roboShield.resetEncoder(ENCODER_0_PIN);
-    roboShield.resetEncoder(ENCODER_1_PIN);
-  }
-  if (Emotor == 2) {
-    roboShield.setMotor(Emotor, 50);
-    if (roboShield.readEncoder(ENCODER_0_PIN) > distance) {
-      roboShield.setMotor(Emotor, 0);
-      done = 1;
-    }
 
-  }
-  else if (Emotor == 3) {
-    roboShield.setMotor(Emotor, 50);
-    if (roboShield.readEncoder(ENCODER_1_PIN) > distance) {
-      roboShield.setMotor(Emotor, 0);
-      done = 1;
-    }
-  }
-  else
-    Serial.print("Your an idiot");
-  return done;
+  return 1;
 }
-*/
+
+void driveDistance(int distance)
+{
+  while(rs.readEncoder(ENCODER_1_PIN) < distance || rs.readEncoder(ENCODER_0_PIN) < distance) {
+      rs.setMotor(rightMotor, -12);
+      rs.setMotor(leftMotor, 13);
+    }
+    rs.setMotor(rightMotor, 0);
+    rs.setMotor(leftMotor, 0);
+
+    Serial.print("You're an idiot");
+}
+
 
 void turnLeft() {
     rs.resetEncoder(ENCODER_0_PIN);
@@ -145,7 +141,7 @@ void turnAround() {
     rs.resetEncoder(ENCODER_0_PIN);
     rs.resetEncoder(ENCODER_1_PIN);
 
-    unsigned int distance = 1215;
+    unsigned int distance = 1220;
     
     while(rs.readEncoder(ENCODER_1_PIN) < distance || rs.readEncoder(ENCODER_0_PIN) < distance) {
       rs.setMotor(rightMotor, 20);
@@ -157,18 +153,39 @@ void turnAround() {
 
 
 void loop() {
-  //if(rs.buttonPressed() == 1){
+  static byte position = 0;
+  if(rs.buttonPressed() == 1){
     rs.lcdClear();
     rs.lcdPrintf("Following Line.");
+    unsigned int sensors[8];
       while(1){
-        /*unsigned int sensors[SENSOR_COUNT];               
-        int position = qtr.readLine(sensors);
-        Serial.println(position - 3350); */
-        //turnRight();
-        //FollowLine(0, position);
-        Serial.println(rs.buttonPressed());
-        delay(200);
+        turnAround();
+        delay(500);
+        while(FollowLine(qtr.readLine(sensors), sensors)) {
+          ;
+        }
+
+        delay(500);
+        turnRight();
+        delay(500);
+        while(FollowLine(qtr.readLine(sensors), sensors)) {
+          ;
+        }
+        delay(500);
+        turnAround();
+        delay(500);
+        while(FollowLine(qtr.readLine(sensors), sensors)) {
+          ;
+        }
+        delay(500);
+        turnLeft();
+        delay(500);
+        while(FollowLine(qtr.readLine(sensors), sensors)) {
+          ;
+        }
+        delay(5000);
+      }
       }
     }
-  //}
+ // }
 //}
